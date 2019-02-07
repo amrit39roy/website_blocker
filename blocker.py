a39e1e8c84d379b9d2cd8702919544bf5952f425
amrit39roy <47436084+amrit39roy@users.noreply.github.com>
2019-02-08 05:06:35 +0530

    Add files via upload

---
+++ blocker.py
@@ -0,0 +1,31 @@
+import time
+from datetime import datetime as dt
+
+hosts_temp="hosts"
+hosts_path=r"C:\Windows\System32\drivers\etc\hosts"
+redirect="127.0.0.1"
+website_list=["www.hotmail.com","hotmail.com","www.rediff.com","rediff.com"]
+
+while True:
+    if 4<=dt.now().hour<=5:
+        print("Working...")
+        with open(hosts_path,'r+') as file:
+            content=file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect+" "+website+"\n")
+
+    else:
+        with open(hosts_path,'r+')as file:
+            content=file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
+            print("fun")
+    time.sleep(5)    
+
+ 
